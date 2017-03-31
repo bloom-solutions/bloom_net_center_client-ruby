4a454008033331a74cbebf0555cf805cbf012fb6
Ramon Tayag <ramon.tayag@gmail.com>
2017-03-31 09:45:05 +0800

    FindTxnResponse: `txn` is nil (no longer raises exception) when the response is invalid
    fixes [#4]

--- lib/bloom_net_center_client/responses/txn_response.rb
+++ lib/bloom_net_center_client/responses/txn_response.rb
@@ -6,10 +6,13 @@ module BloomNetCenterClient
     private
 
     def default_txn
+      attribute_params = body[:data][:attributes]
+      return nil if attribute_params.nil?
+
       attributes = Txn::ATTRS.each_with_object({}) do |attr, hash|
         attr_dasherized = attr.to_s.dasherize
         hash[attr] = body[:data][attr_dasherized] ||
-          body[:data][:attributes][attr_dasherized]
+          attribute_params[attr_dasherized]
       end
       BloomNetCenterClient::Txn.new(attributes)
     end
